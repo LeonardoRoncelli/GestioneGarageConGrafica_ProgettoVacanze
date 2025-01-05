54412bc27e14666563d80323cb9762d5dbf98f8f
Roncelli <leonardoroncelli@gmail.com>
2025-01-05 13:33:42 +0100

    Metodi classe HelloController

--- src/main/java/com/example/gestionegaragecongrafica_progettovacanze/HelloController.java
+++ src/main/java/com/example/gestionegaragecongrafica_progettovacanze/HelloController.java
@@ -23,7 +23,56 @@ public class HelloController {
     @FXML
     private Button ricercaButton;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button numBoxOccupatiButton;
+    @FXML
+    private Button numBoxLiberiButton;
+
+    private Garage garage;
+    public void initialize() {
+        garage = new Garage();
+
+        parcheggiaButton.setOnAction(event -> parcheggiaVeicolo());
+        ricercaButton.setOnAction(event -> ricercaVeicolo());
+        numBoxOccupatiButton.setOnAction(event -> mostraBoxOccupati());
+        numBoxLiberiButton.setOnAction(event -> mostraBoxLiberi());
+    }
+    private void parcheggiaVeicolo() {
+        String targa = targaField.getText();
+        String marca = marcaField.getText();
+        String modello = modelloField.getText();
+        String tempoText = tempoField.getText();
+        String boxText = boxField.getText();
+        if (targa.isEmpty() || marca.isEmpty() || modello.isEmpty() || tempoText.isEmpty() || boxText.isEmpty()) {
+            mostraMessaggio("Errore", "Tutti i campi devono essere compilati!", AlertType.ERROR);
+            return;
+        }
+        try {
+            double tempo = Double.parseDouble(tempoText);
+            int numBox = Integer.parseInt(boxText);
+
+            Auto auto = new Auto(targa, marca, modello, garage.calcolaPrezzo(tempo), "suv");
+
+            garage.parcheggiaBoxSpecifico(numBox, auto);
+            risultatoLabel.setText("Veicolo parcheggiato con successo!");
+        } catch (NumberFormatException e) {
+            mostraMessaggio("Errore", "Inserisci valori validi per il tempo e il numero del box.", AlertType.ERROR);
+        }
+    }
+    private void ricercaVeicolo() {
+        String targa = targaField.getText();
+        if (targa.isEmpty()) {
+            mostraMessaggio("Errore", "Inserisci una targa da cercare!", AlertType.ERROR);
+            return;
+        }
+        garage.ricercaConTarga(targa);
+        risultatoLabel.setText("Veicolo trovato!");
+    }
+    private void mostraBoxOccupati() {
+        int boxOccupati = garage.numBoxOccupati();
+        risultatoLabel.setText("Box occupati: " + boxOccupati);
+    }
+    private void mostraBoxLiberi() {
+        int boxLiberi = garage.numBoxLiberi();
+        risultatoLabel.setText("Box liberi: " + boxLiberi);
     }
 }
