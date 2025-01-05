2a3e3805d8c5029bb51b21b806d762ea1ee26cbd
Roncelli <leonardoroncelli@gmail.com>
2025-01-05 13:32:22 +0100

    Attributi classe HelloController

--- src/main/java/com/example/gestionegaragecongrafica_progettovacanze/HelloController.java
+++ src/main/java/com/example/gestionegaragecongrafica_progettovacanze/HelloController.java
@@ -1,12 +1,27 @@
 package com.example.gestionegaragecongrafica_progettovacanze;
-
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private TextField targaField;
+    @FXML
+    private TextField marcaField;
+    @FXML
+    private TextField modelloField;
+    @FXML
+    private TextField tempoField;
+    @FXML
+    private TextField boxField;
+    @FXML
+    private Label risultatoLabel;
+    @FXML
+    private Button parcheggiaButton;
+    @FXML
+    private Button ricercaButton;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
